7e27de74ea1c79a2d081f247cdd2f0d46d3a8320
qiuzhanghua <qiuzhanghua@icloud.com>
2023-04-27 08:38:41 +0800

    pnpm up

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { render } from "solid-js/web";
 import "./style.css";
 import "virtual:uno.css";
-
 import App from "./App";
 
 render(() => <App />, document.getElementById("app") as HTMLElement);
